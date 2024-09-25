c1a07b4dd3c12efba511b5e838360ca24fbb2d70
Jaron Kenyon <jjkenyon777@gmail.com>
2024-09-24 23:51:12 -0700

    animation speed

--- components/navbar.js
+++ components/navbar.js
@@ -84,7 +84,7 @@ export default function NavBar() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.5 }}
     >
       <div className="relative h-20 w-20">
         <Image
