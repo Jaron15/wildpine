9450e3cf302f456358f49e573d182a499e1b45be
Jaron Kenyon <jjkenyon777@gmail.com>
2024-10-11 19:04:23 -0700

    apostrophe correction

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -14,7 +14,7 @@ const AboutSection = () => {
           <div className='bg-gradient-to-r from-[#6B705C] to- w-1/2 h-0.5 mt-4 mb-2'></div>
           <div className='my-2 sm:my-4 flex flex-col items-center'>
           <p className="text-lg text-center  mb-2 flex justify-center font-medium">
-          At Wild Pine Coffee, we believe in more than just great coffee — we create a space for meaningful connections. From our premium, handpicked beans to our naturally derived energy drinks, every cup is crafted with care. Whether you're catching up with friends or working on your next big idea, our cozy atmosphere is the perfect place to make memories. Stop by, grab a drink, and experience the essence of Wild Pine!
+          At Wild Pine Coffee, we believe in more than just great coffee — we create a space for meaningful connections. From our premium, handpicked beans to our naturally derived energy drinks, every cup is crafted with care. Whether you&apos;re catching up with friends or working on your next big idea, our cozy atmosphere is the perfect place to make memories. Stop by, grab a drink, and experience the essence of Wild Pine!
           </p>
           <div className="text-center">
             <Link href="/about" className="text-[#FF7F50] underline hover:text-[#d16943] ease-in-out duration-300 mb-4">Learn more about us →</Link>
